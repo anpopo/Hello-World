f556b99ac6cc9450c6f900c3db457fb088a0047a
anpopo <65079652+anpopo@users.noreply.github.com>
2020-06-29 20:19:32 +0900

    Create Gugudan

--- src/Gugudan.java
+++ src/Gugudan.java
@@ -1,4 +1,12 @@
 
 public class Gugudan {
-
+	public static void main(String[] args) {
+		// 2단 구현
+		int i = 1
+		for (i <= 10) {
+			System.out.println("2 * " i = 2 * i );
+		}
+		// 3단 구현
+		
+	}
 }
