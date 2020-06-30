8a8ad64092a64514ec5920cab4200a11a31b4472
anpopo <65079652+anpopo@users.noreply.github.com>
2020-06-30 13:04:19 +0900

    Create Gugudan with method

--- src/Gugudan.java
+++ src/Gugudan.java
@@ -1,20 +1,44 @@
+// 구구단 생성 프로젝트 진행
 import java.util.Scanner;
 
+
 public class Gugudan {
+	 static String[] calculator(int times) {
+		String[] guguList = new String[9];
+		for (int i = 0; i < guguList.length; i++) {
+			guguList[i] = String.format("%d * %d = %d", times, i + 1, times * i);
+		}
+		return guguList;
+	}
+	
+	 static void printer(String[] list) {
+		 for (int i = 0;  i < list.length; i++) {
+			 System.out.println(list[i]);
+		 }
+	 }
+	
+	
+	
 	public static void main(String[] args) {
-		
-		// 출력을 원하는 단의 구구단을 입력받기
-		System.out.println("구구단 중 출력할 단은? : ");
-		Scanner scanner = new Scanner(System.in);
-		int num = scanner.nextInt();
-		System.out.println("number : " + num);
-		
-		if (num < 2 || num > 9) {
-			System.out.println("2이상, 9이하의 값만 입력할 수 있습니다.");
-		} else {
-			for (int i = 1; i < 10; i++) {
-				System.out.printf("%d * %d = %d%n", num, i, num * i);
+		System.out.println("===============================");
+		System.out.println("|구구단 생성 프로그램에 오신 것을 환영 합니다.|");
+		System.out.println("|    원하시는 구구단수를 입력해 주세요!   |");
+		System.out.println("===============================");
+		while (true) {
+			Scanner scanner = new Scanner(System.in);
+			int num = scanner.nextInt();
+			if (num < 2 || num > 9) {
+				System.out.println("2와 9사이의 숫자를 넣어주세요~");
+			} else {
+				System.out.println("단수 : " + num);
+				System.out.println(num + "단을 출력합니다 :)");
+				printer(calculator(num));
+				System.out.println("원하시는 숫자를 입력해주세요!");
+				continue;
 			}
 		}
+		
+		
+		
 	}
 }
