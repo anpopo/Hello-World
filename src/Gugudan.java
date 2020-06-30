444ca2de3cce8e10abde12ad330b89cc566222f5
anpopo <65079652+anpopo@users.noreply.github.com>
2020-06-30 10:18:50 +0900

    Create Gugudan function

--- src/Gugudan.java
+++ src/Gugudan.java
@@ -1,13 +1,20 @@
+import java.util.Scanner;
 
 public class Gugudan {
 	public static void main(String[] args) {
-		// 2단 구현
-		for (int i = 1; i < 10; i++) {
-			System.out.printf("2 * %d = %d%n", i, 2 * i );
-		}
-		// 3단 구현
-		for (int i = 1; i < 10; i++) {
-			System.out.printf("3 * %d = %d%n", i, 3 * i);
+		
+		// 출력을 원하는 단의 구구단을 입력받기
+		System.out.println("구구단 중 출력할 단은? : ");
+		Scanner scanner = new Scanner(System.in);
+		int num = scanner.nextInt();
+		System.out.println("number : " + num);
+		
+		if (num < 2 || num > 9) {
+			System.out.println("2이상, 9이하의 값만 입력할 수 있습니다.");
+		} else {
+			for (int i = 1; i < 10; i++) {
+				System.out.printf("%d * %d = %d%n", num, i, num * i);
+			}
 		}
 	}
 }
